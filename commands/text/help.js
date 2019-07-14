3dffaf3b4e1451b9fd48f6c8f89ee64fc2a279b3
Dan <danphi@gmail.com>
2019-07-13 20:43:13 -0700

    add link to site in help

--- commands/text/help.js
+++ commands/text/help.js
@@ -2,6 +2,8 @@ export default function help(db, discord, msg) {
   let m = '**Warrior Bot**\n';
   m += 'Recruit warriors.  Challenge other players.  Attack other Discord guilds and steal their riches.\n';
   m += '\n';
+  m += 'http://warriorbot.io/\n';
+  m += '\n';
   m += 'Type **!commands** to see available commands.\n';
   msg.channel.send(m);
 }
